264a59d7d03ee771fe991261dfbf477912ebd015
Juan Camilo Velásquez Vanegas <juancamilo---142@hotmail.com>
2020-02-11 21:43:10 -0500

    Nav bar implemented using AppBar from Material-UI.

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
-import './NavBar.scss';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import { MAIN_COLOR } from 'constants/Colors';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  appBar: {
+    background: MAIN_COLOR,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+});
 
 export class NavBar extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div className="navBar">
-        <div className="title">Movies/Genres</div>
-        <div className="buttons">
-          <Link className="btn white-btn" to="/">Home</Link>
-          <Link className="btn white-btn" to="/movies">Movies</Link>
-          <Link className="btn white-btn" to="/genres">Genres</Link>
-        </div>
+      <div className={classes.root}>
+        <AppBar position="static" className={classes.appBar}>
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Movies/Genres
+            </Typography>
+            <Button color="inherit" component={Link} to="/">Home</Button>
+            <Button color="inherit" component={Link} to="/movies">Movies</Button>
+            <Button color="inherit" component={Link} to="/genres">Genres</Button>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default NavBar;
+NavBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(NavBar);
