d45a910d5462d4b6d67005216e9e1671ce072928
Juan Camilo Velásquez Vanegas <juancamilo---142@hotmail.com>
2020-02-17 20:42:56 -0500

    Home component updated with new icons and information.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,58 @@
 import React, { Component } from 'react';
-import './Home.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import MovieIcon from '@material-ui/icons/Movie';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+  },
+  paper: {
+    padding: '1rem',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  icon: {
+    fontSize: '3rem',
+  },
+});
 
 export class Home extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div className="homeContainer">
-        <h1>Welcome</h1>
-        <h3>Start navigating using navbar buttons</h3>
+      <div className={classes.root}>
+        <Paper className={classes.paper} elevation={3} >
+          <MovieIcon className={classes.icon} />
+          <b>Manage Movies</b>
+        </Paper>
+        <Paper className={classes.paper} elevation={3} >
+          <ListAltIcon className={classes.icon} />
+          <b>List Genres</b>
+        </Paper>
+        <Paper className={classes.paper} elevation={3} >
+          <DoubleArrowIcon className={classes.icon} />
+          <b>More...</b>
+        </Paper>
       </div>
     );
   }
 }
 
-export default Home;
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Home);
