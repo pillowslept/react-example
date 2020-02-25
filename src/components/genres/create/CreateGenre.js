f210fc322f86b270e2fe5f10fa2eebaf480b7fb3
Juan Camilo Velásquez Vanegas <juancamilo---142@hotmail.com>
2020-02-24 20:06:39 -0500

    Dialog component for genre creation.

---
+++ src/components/genres/create/CreateGenre.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import Divider from '@material-ui/core/Divider';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export class CreateGenre extends Component {
+  constructor() {
+    super()
+    this.state = {
+      genre: {
+        name: '',
+      },
+    }
+    this.process = this.process.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  process() {
+    fetch('http://localhost:3035/api/nodejs-example/genre', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state.genre),
+    })
+    .then(() => {
+      this.props.openCloseModal();
+    })
+    .catch(console.log)
+  }
+  handleChange = (event) =>{ 
+    this.setState({ genre: { name: event.target.value } });
+  }
+  render() {
+    const { isOpen, openCloseModal } = this.props;
+    const width = 'xs';
+
+    return (
+      <Dialog
+        fullWidth
+        maxWidth={width}
+        open={isOpen}
+        onClose={openCloseModal}
+        aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Create</DialogTitle>
+        <Divider />
+        <DialogContent>
+          <TextField
+            autoFocus
+            value={this.state.name} onChange={this.handleChange}
+            margin="dense"
+            id="name"
+            label="Genre name"
+            type="text"
+            fullWidth
+            autoComplete="off"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={openCloseModal} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={this.process} color="primary">
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
+
+export default CreateGenre;
